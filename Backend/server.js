d47278732acb41f0e0f8ae0f8ea265632bc91bef
HassanAdemuyiwa <ademuyiwahassan68@gmail.com>
2021-06-10 11:41:27 +0100

    env added

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,8 @@
 import express from 'express'
+import dotenv from 'dotenv';
+dotenv.config();
+
+// const connection_url =`mongodb+srv://admin:${process.env.DB_PASSWORD}@cluster0.pcpxh.mongodb.net/tinderdb?retryWrites=true&w=majority`
 
 
 const app = express();
@@ -7,7 +11,6 @@ const PORT = process.env.PORT || 8080;
 
 
 app.get('/', (req, res) => res.status(200).send('Hello world') );
-
 
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
